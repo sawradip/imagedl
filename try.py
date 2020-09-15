1eb5aabcf2346d7e0db08903569915fe683f07f3
sawradip <sawradip0@gmail.com>
2020-09-15 16:43:42 +0600

    Update try.py

--- try.py
+++ try.py
@@ -2,10 +2,10 @@ from googleimage import GoogleImage
 from googlereverseimage import GoogleReverseImage
 
 
-#block 1
+#Block 1
 #dl = GoogleImage(headless= True)
 #dl.download(search_text = 'cute_babies', n_images= 5, save_folder = "babies")
 
-#block 2
-dl = GoogleReverseImage(headless= True)
-dl.download(search_image= r'cute_baby.jpg', n_images= 5, save_folder = "babies")
+#Block 2
+#dl = GoogleReverseImage(headless= True)
+#dl.download(search_image= r'cute_baby.jpg', n_images= 5, save_folder = "babies")
